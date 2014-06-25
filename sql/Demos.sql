bb1b092648923389b48e92197e4aa54c806064b2
36122-isel <36122@alunos.isel.ipl.pt>
2014-06-25 17:29:34 +0100

    Procedimentos alienas 2(g, h, i, j) Adicionado ficheiro para execução de demos (execução de procedimentos armazenados do modelo)

---
+++ sql/Demos.sql
@@ -0,0 +1,45 @@
+/************************************************
+* Demos
+************************************************/
+Use SI2_1314v_TP;
+
+/************************************************
+* ALINEA 2.a
+************************************************/
+EXEC InserirEmpresa 600016234, 'Instituto Superior de Engenharia de Lisboa', 'Rua Conselheiro Emídio Navarro 1, 1959-007 Lisboa'
+
+EXEC InserirAreaIntervencao @AreaID OUT, 'Assistência Técnica'
+
+/************************************************
+* ALINEA 2.b
+************************************************/
+EXEC EditarEmpresa 600016234, 'Instituto Superior de Engenharia de Lisboa', 'Rua Conselheiro Emídio Navarro 1, 1959-007 Lisboa'
+
+/************************************************
+* ALINEA 2.c
+************************************************/
+/* Verificar disparo do gatilho (Ocorrencia_alterada) quando uma Ocorrencia e alterada */
+
+Exec ReportarOcorrencia 'trivial', 501510184, 1, 1, 'A', @OcorID OUT
+
+/************************************************
+* ALINEA 2.g
+************************************************/
+EXEC ListarTotalOcorrenciaDasEmpresas
+
+/************************************************
+* ALINEA 2.h
+************************************************/
+EXEC ListarOcorrenciasEmIncumprimento
+
+/************************************************
+* ALINEA 2.i
+************************************************/
+EXEC ListarOcorrenciasEmIncumprimento 'Manutenção Extintores'
+
+/************************************************
+* ALINEA 2.j
+************************************************/
+EXEC ListarFuncionariosCoordenadoresSemOcorrenciasCriticas
+
+
