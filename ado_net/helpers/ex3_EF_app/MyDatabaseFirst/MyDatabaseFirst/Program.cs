7ae2a72695b46c9628dd63c29338295abbd767f4
Pedro Marinho <a36122@alunos.isel.pt>
2014-06-30 10:19:26 +0100

    Auxiliares para alinea 3 e 4c

---
+++ ado_net/helpers/ex3_EF_app/MyDatabaseFirst/MyDatabaseFirst/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyDatabaseFirst
+{
+    class Program
+    {
+        public static void WriteMenu()
+        {
+            Console.WriteLine("Select an operation:");
+            Console.WriteLine("1 - Add Accounts");
+            Console.WriteLine("2 - Update client name");
+            Console.WriteLine("3 - Update account balance");
+            Console.WriteLine("4 - Add account for existing client");
+            Console.WriteLine("5 - Delete account");
+            Console.WriteLine("0 - Exit application");
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            WriteMenu();
+            var operation = Convert.ToInt32(Console.ReadLine());
+            while (!operation.Equals(0))
+            {
+                switch (operation)
+                {
+                    case 1:
+                        AddContas();
+                        break;
+                    case 2:
+                        UpdateClienteName("Pedro");
+                        break;
+                    case 3:
+                        UpdateSaldoConta(3000, 2000);
+                        break;
+                    case 4:
+                        AddContaExistingCliente(1);
+                        break;
+                    case 5:
+                        DeleteConta(3000);
+                        break;
+                    case 0:
+                        return;
+                }
+                WriteMenu();
+                operation = Convert.ToInt32(Console.ReadLine());
+            }
+            
+           
+           
+/*
+ *
+ * 
+Nota: devem usar blocos
+using
+! E devem criar um método para cada exercício.
+a.
+Exercício 1 : Adicionar ao ContasContex um novo cli
+ente e uma nova conta para esse
+cliente
+b.
+Exercício 2 : Alterar o nome de um cliente da BD
+c.
+Exercício 3: Alterar o saldo de uma conta
+d.
+Exercício 4: Adicionar uma nova conta para um cliente que já exista.
+e.
+Exercício 5: Apagar uma conta e/ou um cliente da BD Neste momento é possível trabalhar com o ContasContext e usar as instâncias das
+entidades do Modelo. 
+
+ * Nota 
+ * */
+
+            // var c = new Cliente() {nome = "novo cliente"};
+            // c.Contas.Add(new Conta() {id = 3000, saldo = 1000});
+
+            /*
+         * (TPC) Testar cenários de concorrência entre actuali
+zações via Entity Framework e actualizações
+directamente na BD (com Management Studio).
+Pergunta: Como detectar que os valores da BD foram
+alterados? E como gerir os conflitos
+         */
+
+
+
+        }
+
+        #region Adicionar ao ContasContex um novo cliente e uma nova conta para esse cliente
+        /// <summary>
+        /// Adicionar ao ContasContex um novo cliente e uma nova conta para esse cliente
+        /// </summary>
+        public static void AddContas()
+        {
+            using (var ctx = new ContasContext())
+            {
+                var cl = new Cliente();//{name = "António"};
+                var co = new Conta();//{id = 3000, saldo = 1000};
+                Console.WriteLine("Creating new account... ");
+                Console.WriteLine("Write owner name : ");
+                cl.name = Console.ReadLine();
+
+                Console.WriteLine("Write account id : ");
+                co.id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Write balance : ");
+                co.saldo = Convert.ToDecimal(Console.ReadLine());
+
+                
+                
+                cl.Contas.Add(co);
+                ctx.Clientes.Add(cl);
+                ctx.SaveChanges();
+
+            }
+        }
+        #endregion
+
+        #region Alterar o nome de um cliente da BD
+        /// <summary>
+        /// Alterar o nome de um cliente da BD
+        /// </summary>
+        public static void UpdateClienteName(string cliName)
+        {
+            using (var ctx = new ContasContext())
+            {
+                Console.WriteLine("Updating cliente name... ");
+                Console.WriteLine("Write owner name : ");
+                cliName = Console.ReadLine();
+                Console.WriteLine("Write account id : ");
+                var contaId =  Convert.ToInt32(Console.ReadLine());
+               
+                var cli = (from c in ctx.Clientes
+                               where c.id == contaId
+                               select c).SingleOrDefault();
+
+                if (cli == null) return;
+                cli.name = cliName;
+
+                //ctx.Clientes.Add(cli);
+                ctx.SaveChanges();
+            }
+        }
+        #endregion
+
+
+        #region Alterar o saldo de uma conta
+        /// <summary>
+        /// Alterar o saldo de uma conta
+        /// </summary>
+        public static void UpdateSaldoConta(int contaId, decimal novoSaldo)
+        {
+            using (var ctx = new ContasContext())
+            {
+                Console.WriteLine("Updating account balance... ");
+                Console.WriteLine("Write balance : ");
+                novoSaldo = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Write account id : ");
+                contaId = Convert.ToInt32(Console.ReadLine());
+                var conta = (from c in ctx.Contas
+                               where c.id == contaId
+                               select c).SingleOrDefault();
+
+                if (conta == null) return;
+                conta.saldo = novoSaldo;
+
+                //ctx.Clientes.Add(cli);
+                ctx.SaveChanges();
+            }
+        }
+        #endregion
+
+
+        #region Adicionar uma nova conta para um cliente que já exista.
+        /// <summary>
+        /// Adicionar uma nova conta para um cliente que já exista.
+        /// </summary>
+        public static void AddContaExistingCliente(int clienteId)
+        {
+            using (var ctx = new ContasContext())
+            {
+                Console.WriteLine("Add new account to existing client... ");
+                Console.WriteLine("Write account id : ");
+                var contaId = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Write owner id : ");
+                clienteId = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Write balance : ");
+                var novoSaldo = Convert.ToDecimal(Console.ReadLine());
+                
+
+                var cliente = (from c in ctx.Clientes
+                             where c.id == clienteId
+                               select c).SingleOrDefault();
+
+                if (cliente == null) return;
+                cliente.Contas.Add(new Conta { id = contaId, saldo = novoSaldo });
+
+                ctx.SaveChanges();
+            }
+        }
+        #endregion
+
+        
+      #region Apagar uma conta e/ou um cliente da BD Neste momento é possível trabalhar com o ContasContext e usar as instâncias das entidades do Modelo. 
+        /// <summary>
+        /// Apagar uma conta e/ou um cliente da BD Neste momento é possível trabalhar com o ContasContext e usar as instâncias das entidades do Modelo. 
+        /// </summary>
+        public static void DeleteConta(int contaId)
+        {
+            using (var ctx = new ContasContext())
+            {
+                Console.WriteLine("Delete account... ");
+                Console.WriteLine("Write account id : ");
+                contaId = Convert.ToInt32(Console.ReadLine());
+
+                var conta = (from c in ctx.Contas
+                               where c.id == contaId
+                               select c).SingleOrDefault();
+
+                if (conta == null) return;
+                ctx.Contas.Remove(conta);
+
+                //ctx.Clientes.Add(cli);
+                ctx.SaveChanges();
+            }
+        }
+        #endregion
+    }
+}
---
+++ ado_net/helpers/ex4_xml_4c/app/app/Program.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace app
+{
+    class Program
+    {
+
+        // alineas a
+        internal static SqlConnection CreateConnection()
+        {
+            //SqlConnection MyConnection = new SqlConnection("server=(local)\\PM-PC;database=Si_2_sandbox;Integrated Security=SSPI;");
+            return new SqlConnection("Data Source=PM-PC;Database=si_2_sandbox;Integrated Security=true;");
+        }
+
+        // alineas b
+        internal static bool TestConnetion()
+        {
+            SqlConnection conn = CreateConnection();
+
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Ligação com o servidor de SQLServer efectuada!");
+                conn.Close();
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+
+        // alinea c)
+        internal static int ExecuteInsertCommand(SqlConnection conn)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO Carta (id, nome, valor) values(4, 'carta4', 200)";
+           
+            conn.Open();
+            var res = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return res;
+        }
+
+        // alinea d)
+        static SqlDataReader ExecuteSelectCommand(SqlConnection conn)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT id, nome, valor FROM Carta";
+            
+           
+            var res = cmd.ExecuteReader();
+            
+            
+            return res;
+        }
+
+        // alinea d)
+        static void ListResultSet(SqlDataReader rs)
+        {
+            while (rs.Read())
+            {
+                Console.Write("id:" + rs.GetInt32(0));
+                Console.Write(" ,");
+                Console.WriteLine("Nome:" + rs.GetString(1));
+                Console.Write(" ,");
+                Console.WriteLine("Valor:" + rs.GetInt32(2));
+            }
+        }
+
+        // alinea e)
+        static int ExecuteInsertInputParameters(SqlConnection conn, Int32 id, String nome, Int32 valor)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO Carta (id, nome, valor) values(@id, @nome, @valor)";
+            
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@nome", nome);
+            cmd.Parameters.AddWithValue("@valor", valor);
+
+            conn.Open();
+            var res = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return res;
+        }
+
+        // alinea e) iii)
+        static int ExecuteInsertInputNullParameter(SqlConnection conn, Int32 id, Int32 nome, DBNull valor)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO T (id, value, design) values(@id, @value, @design)";
+
+            //SqlParameter p = new SqlParameter("@id", SqlDbType.Int);
+
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@value", nome);
+            cmd.Parameters.AddWithValue("@design", valor);
+
+            conn.Open();
+            var res = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return res;
+        }
+
+
+        void oldMain()
+        {
+            //// alinea a) e b) --> estabelecer uma ligação à base de dados
+            //TestConnetion();
+
+            //// alinea c) --> executar um comando insert
+            //Console.WriteLine("Número de linhas inseridas: {0}", ExecuteInsertCommand(CreateConnection()));
+
+            //// alinea d) --> executar um comando que retorna um ResulSet --> imprimir linhas na consola
+            //SqlConnection conn = CreateConnection();
+            //conn.Open();
+            //ListResultSet(ExecuteSelectCommand(conn));
+            //conn.Close();
+
+            //// alinea e) --> criar um comando com parameters
+            //Console.WriteLine("Número de linhas inseridas: {0}", ExecuteInsertInputParameters(CreateConnection(), 5, "carta5", 500));
+
+            //// alinea e) iii)--> criar um comando com parameters que inser um NULL na tabela
+            //Console.WriteLine("Número de linhas inseridas: {0}", ExecuteInsertInputNullParameter(CreateConnection(), 5, 5, DBNull.Value));
+
+            //// alinea f) --> criar um comando com parameters
+        }
+
+        /// <summary>
+        /// Data Adapter
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+           
+
+
+            // Data Adapter-------------------------------------------
+            SqlConnection conn = CreateConnection();
+            //conn.Open();
+            String selectStr = "SELECT id, nome, valor FROM Carta";
+
+
+       //     CreateDataAdapter(conn, selectStr);
+        //    FillDataSetWithDatatable();
+         //   ShowDatatableContent();
+
+            ReadXmlToDataset();
+            conn.Close();
+
+
+          }
+
+        private static void CreateDataAdapter(SqlConnection conn, string selectStr)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(selectStr, conn);
+            //DataTable dt = new DataTable();
+            DataSet ds = new DataSet();
+            //da.Fill(dt);
+            da.Fill(ds, "T3"); // Versão 1 da tabela Carta
+            
+            da.Fill(ds, "T2"); // Versão 2 da tabela Carta
+
+            da.Fill(ds);
+            
+            // DataSet colecção de datatables
+
+            //foreach (DataRow row in dt.Rows)
+            //    Console.WriteLine("{0} {1} {2}", row[0], row["nome"], row["valor"]);
+
+
+            
+            da.Fill(ds, "T"); //Versão 3 da tabela Carta mas com nome T no dataset
+          
+
+            Console.WriteLine("Número de tabelas no DataSet {0}", ds.Tables.Count);
+
+            foreach (DataTable t in ds.Tables)
+            {
+                Console.WriteLine("Nome da tabela {0}", t.TableName);
+            }
+            //Console.WriteLine("Nome da tabela {0}", ds.Tables[0].TableName);
+            //Console.WriteLine("Nome da tabela {0}", ds.Tables[1].TableName);
+            foreach (DataRow row in ds.Tables["T"].Rows)
+                Console.WriteLine("{0} {1} {2}", row[0], row[1], row[2]);
+
+
+            // Alterar valor na datatable
+
+            SqlDataAdapter daUpdate = new SqlDataAdapter();
+            daUpdate.SelectCommand = new SqlCommand("SELECT id, tipo FROM T1", conn);
+           // daUpdate.UpdateCommand = new SqlCommand("update carta set valor = 33 where id = 3", conn);
+            SqlCommandBuilder builder = new SqlCommandBuilder(daUpdate);
+            DataSet ds2 = new DataSet();
+
+            ITableMapping tm = daUpdate.TableMappings.Add("T1", "T3");
+            tm.ColumnMappings.Add("id", "novoId");
+            tm.ColumnMappings.
+            //da.Fill(dt);
+            daUpdate.Fill(ds2, "T1"); // Versão 1 da tabela Carta
+
+            // ...
+
+            //builder.GetUpdateCommand();
+            
+            Console.WriteLine("name before change: {0}", ds2.Tables["T1"].Rows[1]["tipo"]);
+            ds2.Tables["T1"].Rows[1]["tipo"] = "F";
+            daUpdate.UpdateCommand = builder.GetUpdateCommand(); // Tabela tem de ter chave
+            daUpdate.Update(ds2, "T1");
+            
+            Console.WriteLine("name after change: {0}", ds2.Tables["T1"].Rows[1]["tipo"]);
+           
+
+            // Converter para XML (Mapping)
+
+            string xmlDS = ds.GetXml();
+
+            // ds.WriteXml("T1.xml", XmlWriteMode.WriteSchema); escreve o esquema XSD
+            ds.WriteXml("T1.xml", XmlWriteMode.IgnoreSchema);
+        }
+
+        private static void ReadXmlToDataset()
+        {
+            DataSet ds2 = new DataSet();
+            using (StreamReader xmlDoc = new StreamReader("T1.xml"))
+            {
+                //Escrevendo no documento
+                ds2.ReadXml(xmlDoc);
+
+                foreach (DataRow row in ds2.Tables["T3"].Rows)
+                    Console.WriteLine("{0} {1} {2}", row[0], row[1], row[2]);
+            }
+        }
+    }
+}
